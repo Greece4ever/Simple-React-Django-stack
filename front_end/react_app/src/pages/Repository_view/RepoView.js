60008edba502e94d21ce737cc10fc2503fbabdf9
Leonidios <programertv633@gmail.com>
2020-08-20 18:46:14 +0300

    Added Progressbar showing percentage of files

--- front_end/react_app/src/pages/Repository_view/RepoView.js
+++ front_end/react_app/src/pages/Repository_view/RepoView.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import {DetailRegExpRepository,matchDetailRegExp,capitalizeFirst,getFileName} from "../../settings";
+import {DetailRegExpRepository,matchDetailRegExp,capitalizeFirst,getFileName,getMaxOccurances,extractMaxOccurances,getMaxPercentage,randomColor} from "../../settings";
 import {useHistory} from "react-router-dom";
 import {DetailRepository,UploadFile} from "../ajax/ajax";
 import Header from "../components/header";
@@ -18,6 +18,11 @@ const RepoView = () => {
     const [fileURI,setFileURI] = useState('');
     const [id,setID] = useState('')
 
+    const [maxOccurances,setMaxOccurances] = useState('');
+    const [extractedOccurances,setExtractedOccurances] = useState([]);
+    const [maxPercentage,setMaxPercentage] = useState('')
+
+
     const[error,setError] = useState('');
 
 
@@ -27,7 +32,6 @@ const RepoView = () => {
     useEffect(() => {
         const href = window.location.href;
         let isMatch = matchDetailRegExp(href,DetailRegExpRepository)
-        console.log(isMatch)
         if(!isMatch[0]) {
             history.push('/404')
             return () => {}
@@ -52,18 +56,38 @@ const RepoView = () => {
             setUser(capitalizeFirst(isMatch[1]))
             setName(response.data.name)
             let x = response.data.files;
-            console.log(x)
             let list = [];
             for (let item in x) 
             {
                 list.push({"name" : getFileName(x[item][0]),"url" : `http://localhost:8000/media/${x[item][0]}`})
             }
             setFiles(list)
+            
+            const PROCCESSED_ARRAY = list.map(item => item.name)
+
+            //Evaluate
+            let tmp_max_occurances  = getMaxOccurances(PROCCESSED_ARRAY);
+            let tmp_extracted_occurances = extractMaxOccurances(tmp_max_occurances);
+            let tmp_max_percentage = getMaxPercentage(tmp_extracted_occurances);
+            
+
+            // Set the state to the evaluated values
+            setMaxOccurances(tmp_extracted_occurances);
+            setExtractedOccurances(tmp_extracted_occurances);
+            setMaxPercentage(tmp_max_percentage);
+
         }
         fetchRepository();
 
     },[])
- 
+
+    const handleClick = () => {
+        console.log(maxOccurances);
+        console.log(extractedOccurances);
+        console.log(maxPercentage);
+    }
+
+
     async function handleFile()  {
         let element = document.getElementById('file');
         var formData = new FormData();
@@ -83,7 +107,7 @@ const RepoView = () => {
         console.log(response)
 
         if(response.data.error)
-        {setError(<div class="alert alert-danger" role="alert">{response.data.error}</div>
+        {setError(<div className="alert alert-danger" role="alert">{response.data.error}</div>
       );return () =>{}}
         
         setFiles(prev => [...prev,{name : response.data.success.name,"url" : `http://localhost:8000${response.data.success.url}`}])
@@ -94,20 +118,28 @@ const RepoView = () => {
         <div>
             <Header />
             <div style={{"marginBottom" : "0"}} className="jumbotron jumbotron-fluid">
-                <div style={{"textAlign" : "center"}} className="container">
-                    <h1 className="display-5"><b style={{"color" : "#0f618c"}}>{user}</b>'s Repositories</h1>
-                    <p style={{"marginTop" : "20px"}} className="lead"><button className="btn btn-success">View More Repositories</button></p>
+                <div  className="container">
+                <div style={{textAlign : "center",marginBottom : "50px",fontSize : "50px",fontFamily : "'Russo One', sans-serif"}}>
+                    Repository <b style={{"color" : "green"}}>"{name}"</b> by <b style={{"color" : "#25808e"}}>{user}</b>
+                </div>
+                    <div style={{"width" : "100%"}} className="progress">
+                        {extractedOccurances.map(item => (<div className={`progress-bar ${item.color}`} role="progressbar" style={{width: item.precentage + "%"}} aria-valuenow="15" aria-valuemin="0" aria-valuemax="100">{`${item.precentage}% - ${item.file == "Other" ? "Other" : `.${item.file.toUpperCase()}`}`}</div>))}
+                        {/* <div className="progress-bar" role="progressbar" style={{width: "15%"}} aria-valuenow="15" aria-valuemin="0" aria-valuemax="100">15% .PY</div>
+                        <div className="progress-bar bg-success" role="progressbar" style={{width: "30%"}} aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">30% .JS</div>
+                        <div className="progress-bar bg-info" role="progressbar" style={{width: "20%"}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">20% .CPP</div>
+                        <div className="progress-bar bg-danger" role="progressbar" style={{width: "35%"}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">35% .CS</div> */}
+                    </div>
                 </div>
             </div>
             {error}
 
             <div style={{"marginTop" : "50px"}} className="container">
                 <div style={{"backgroundColor" : "rgba(0,0,0,.03)",border : "3px solid rgba(0,0,0,.03)",position : "relative"}}>
-                    <i style={{position : "absolute",marginTop : "4px",mariginLeft : "50px"}} class="fas fa-user"></i>
+                    <i style={{position : "absolute",marginTop : "4px",mariginLeft : "50px"}} className="fas fa-user"></i>
                     <div style={{"marginLeft" : "30px"}}>{user}</div>
                     <div style={{"position": "absolute",right : "2px",top : "-5px"}}>
                         <label className="btn btn-default btn-file">
-                            <i class="fas fa-upload"></i><input onChange={() => handleFile()} id="file" type="file" style={{display: "none"}}></input>
+                            <i className="fas fa-upload"></i><input onChange={() => handleFile()} id="file" type="file" style={{display: "none"}}></input>
                         </label>
                     </div>
                 </div>
--- front_end/react_app/src/settings.js
+++ front_end/react_app/src/settings.js
@@ -53,3 +53,86 @@ export const getFileName=(path)=>{
     if (splitted.length ===1){splitted=path.split('\\')}
     return splitted[splitted.length-1];
 }
+
+
+// BAD CODE DO NOT WRITE CODE LIKE THIS \\
+
+
+const colors = ["bg-danger","bg-warning","bg-info","bg-success"]
+
+export const getMaxOccurances = (list) => {
+    let FILE_FORMATS = [];
+    for (let item in list) {
+      let push_item = list[item].split('.')[1]
+      FILE_FORMATS.push(push_item)
+    }
+    let counts = {};
+    FILE_FORMATS.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+    return counts
+}
+
+function sum(args)
+{
+  let total = 0;
+  for (let item in args)
+  {
+    total += args[item];
+  }
+  return total;
+}
+
+export const extractMaxOccurances = (obj) => {
+  let CACHE = [];
+  for (let occurance in obj)
+  {
+    let obj_to_push = Number(obj[occurance]);
+    CACHE.push(obj_to_push)
+  }
+  const TOTAL = sum(CACHE); //Total number of occurances
+  let tmpArray = [];
+  let percentage;
+  let i = 0;
+  for (let item in obj)
+  {
+    if(i==3) {
+        let sam = sum(tmpArray.map(obj => obj.precentage))
+        const res = 100 -sam
+        tmpArray.push({'file' : 'Other',"precentage" : res,color : colors[i]});console.log(tmpArray);return tmpArray;
+    
+    }
+    percentage = Math.ceil((obj[item]/TOTAL) * 100)
+    tmpArray.push({"file" : item,"precentage" : percentage,color : colors[i]})
+    i +=1
+  }
+  return tmpArray;
+}
+
+
+export const getMaxPercentage = (extractedOccurances) => {
+    let VALUES = [];
+    let item_to_push;
+    for (let occurance in extractedOccurances)
+    {
+      if (extractedOccurances[occurance]['file']== 'Other') {continue}
+      item_to_push = extractedOccurances[occurance];
+      VALUES.push(extractedOccurances[occurance]['precentage'])
+    }
+    const HIGHEST_NUM = Math.max(...VALUES);
+    let value_to_check;
+    for (let occurance in extractedOccurances)
+    {
+      value_to_check = extractedOccurances[occurance]['precentage'];
+      console.log(value_to_check)
+      if(value_to_check == HIGHEST_NUM)
+      {
+        return extractedOccurances[occurance]['file'];
+      }
+    }
+  
+  }
+  
+export const randomColor = () => {
+    let r1 = Math.ceil(Math.random() * 255);
+    let r2 = Math.ceil(Math.random() * 255);
+    let r3 = Math.ceil(Math.random() * 255);
+}
